ee075e6b3c5e7b0209af93549588a1fd809ad5d5
Pattabhi Venkanna <144514007+Pattabhi-Venkanna@users.noreply.github.com>
2024-07-10 11:28:10 +0530

    my first commit

---
+++ script/index.js
@@ -0,0 +1,8 @@
+
+
+var typed = new Typed('.professional_text', {
+    strings: ['Java Developer.', 'Web Developer.', 'Full Stack Developer.'],
+    typeSpeed: 120,
+    backSpeed: 50,
+    loop: true
+  });
